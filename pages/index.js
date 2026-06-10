092a6733ea6ed780c5f0e07da65f259edd296122
Courtney Stenstrom <cstenstrom17@gmail.com>
2026-06-10 17:00:25 -0400

    fix source text2

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,7 @@ export default function Home() {
                 >
                   University of New Hampshire Inquiry Journal
                 </a>
+                      </span>
               </p>
             </div>
           </section>
