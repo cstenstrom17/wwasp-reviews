2f2d4edeb5f57cec2b2ed589b3f59c98c408b99c
Courtney Stenstrom <cstenstrom17@gmail.com>
2026-06-11 04:46:01 -0400

    update header2

--- components/layout.js
+++ components/layout.js
@@ -9,18 +9,19 @@ export default function Layout({ children }) {
           
           {/* Typographic Masthead Group */}
           <div className={styles.masthead}>
-            <Link href=\"/\" className={styles.logo}>
+            <Link href="/" className={styles.logo}>
               <h1>The TTI Index</h1>
             </Link>
             <span className={styles.tagline}>
-              A survivor-led public registry
+              A public repository of survivor accounts from the Troubled Teen Industry
             </span>
           </div>
 
           <nav className={styles.nav}>
-            <Link href=\"/\">Home</Link>\n            <Link href=\"/browse\">Browse Programs</Link>
-            <Link href=\"/submit\">Submit Institution</Link>
-            <Link href=\"/resources\">Resources</Link>
+            <Link href="/">Home</Link>
+            <Link href="/browse">Browse Programs</Link>
+            <Link href="/submit">Submit Institution</Link>
+            <Link href="/resources">Resources</Link>
           </nav>
         </div>
       </header>
@@ -33,7 +34,7 @@ export default function Layout({ children }) {
         <p>&copy; 2026 The TTI Index. All rights reserved.</p>
         <p>
           Noticed an error or missing facility? Let me know{" "}
-          <a href=\"mailto:cstenstrom17@gmail.com\" className={styles.footerLink}>
+          <a href="mailto:cstenstrom17@gmail.com" className={styles.footerLink}>
             here.
           </a>
         </p>
