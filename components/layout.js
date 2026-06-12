36cdce6a6c1b37443b3ccc1101b236750e10f885
Courtney Stenstrom <cstenstrom17@gmail.com>
2026-06-12 16:45:53 -0400

    changes

--- components/layout.js
+++ components/layout.js
@@ -1,24 +1,61 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import styles from './layout.module.css'
 
 export default function Layout({ children }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.headerContent}>
           
-          {/* Typographic Masthead Group */}
-          <div className={styles.masthead}>
-            <Link href="/" className={styles.logo}>
-              <h1>Reviewing TTI</h1>
-            </Link>
-          </div>
+          <Link href="/" className={styles.logo}>
+            <h1>Reviewing TTI</h1>
+          </Link>
 
           <nav className={styles.nav}>
             <Link href="/">Home</Link>
-            <Link href="/browse">Browse Programs</Link>
-            <Link href="/submit">Submit Institution</Link>
-            <Link href="/resources">Resources</Link>
+            <Link href="/about">About</Link>
+            
+            {/* DROPDOWN TRIGGER GROUP */}
+            <div 
+              className={styles.dropdownContainer}
+              onMouseEnter={() => setDropdownOpen(true)}
+              onMouseLeave={() => setDropdownOpen(false)}
+            >
+              <button 
+                className={styles.dropdownToggle}
+                aria-expanded={dropdownOpen}
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+              >
+                Explore Archive
+                <span className={`${styles.arrow} ${dropdownOpen ? styles.arrowOpen : ''}`}>
+                  ▼
+                </span>
+              </button>
+
+              {dropdownOpen && (
+                <ul className={styles.dropdownMenu}>
+                  <li>
+                    <Link href="/browse" onClick={() => setDropdownOpen(false)}>
+                      Browse Programs
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/submit" onClick={() => setDropdownOpen(false)}>
+                      Submit a Facility
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/resources" onClick={() => setDropdownOpen(false)}>
+                      Further Resources
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+
           </nav>
         </div>
       </header>
